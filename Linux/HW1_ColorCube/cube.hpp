6534fba0c1dc7184d72d4c89c971f1f2aecc8094
Aesophor <aesophor@mailfence.com>
2018-10-10 21:00:38 +0800

    強迫症發作

--- Linux/HW1_ColorCube/cube.hpp
+++ Linux/HW1_ColorCube/cube.hpp
@@ -8,14 +8,13 @@ class Cube {
 public:
     Cube();
     virtual ~Cube();
-    void Draw();
-
+    
     std::vector<GLfloat*>& vertices();
     std::vector<GLfloat*>& colors();
+    void Draw();
 private:
     std::vector<GLfloat*> vertices_;
     std::vector<GLfloat*> colors_;
-    
     void DrawPolygon(int a, int b, int c, int d); 
 };
 
