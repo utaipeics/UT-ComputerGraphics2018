09c415b60208477cc26040456879b76a9cd14a3f
Aesophor <aesophor@mailfence.com>
2018-10-11 20:46:55 +0800

    Added hw2 3D Gasket (Linux version).

--- Linux/HW2_3D_Gasket/main.cpp
+++ Linux/HW2_3D_Gasket/main.cpp
@@ -1,64 +1,146 @@
+#include <iostream>
 #include <GL/glut.h>
 
-GLfloat vertices[][3] = {
-    {-1.0, -1.0, -1.0}, {1.0, -1.0, -1.0}, 
-    {1.0, 1.0, -1.0}, {-1.0, 1.0, -1.0}, {-1.0, -1.0, 1.0}, 
-    {1.0, -1.0, 1.0}, {1.0, 1.0, 1.0}, {-1.0, 1.0, 1.0}
-};
+using std::cout;
+using std::endl;
 
-GLfloat colors[][3] = {
-    {0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, 
-    {1.0, 1.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}, 
-    {1.0, 0.0, 1.0}, {1.0, 1.0, 1.0}, {0.0, 1.0, 1.0}
-};
+typedef GLfloat point[3];
 
-void drawPolygon(int a, int b, int c, int d) {
-    glBegin(GL_POLYGON);
-    glColor3fv(colors[a]);
-    glVertex3fv(vertices[a]);
-    glColor3fv(colors[b]);
-    glVertex3fv(vertices[b]);
-    glColor3fv(colors[c]);
-    glVertex3fv(vertices[c]);
-    glColor3fv(colors[d]);
-    glVertex3fv(vertices[d]);
+point v[] = {{0.0, 0.0, 1.0}, {0.0, 0.942809, -0.33333}, 
+    {-0.816497, -0.471405, -0.333333}, {0.816497, -0.471405, -0.333333}};
+
+static const int FPS = 60;
+static int n;
+static bool reversing;
+static int x_theta;
+
+void triangle(point a, point b, point c) {
+    glVertex3fv(a);
+    glVertex3fv(b);
+    glVertex3fv(c);
+}
+
+void tetrahedron(point a, point b, point c, point d) {
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0, 0.0, 0.0);
+    triangle(a, b, c);
+    glColor3f(0.0, 0.0, 1.0);
+    triangle(a, d, b);
+    glColor3f(1.0, 1.0, 1.0);
+    triangle(b, d, c);
+    glColor3f(0.0, 1.0, 0.0);
+    triangle(a, c, d);
     glEnd();
 }
 
-void drawColorcube() {
-    drawPolygon(0, 3, 2, 1);
-    drawPolygon(2, 3, 7, 6);
-    drawPolygon(0, 4, 7, 3);
-    drawPolygon(1, 2, 6, 5);
-    drawPolygon(4, 5, 6, 7);
-    drawPolygon(0, 1, 5, 4);
+/*
+ * Triangle subdivision using vertex numbers.
+ * Righthand rule applied to create outward pointing faces.
+ */
+void divide_tetra(point a, point b, point c, point d, int m) {
+    point mid[6];
+
+    if (m > 0) {
+        // Compute six midpoints.
+        for (int i = 0; i < 3; i++) mid[0][i] = (a[i] + b[i]) / 2;
+        for (int i = 0; i < 3; i++) mid[1][i] = (a[i] + c[i]) / 2;
+        for (int i = 0; i < 3; i++) mid[2][i] = (a[i] + d[i]) / 2;
+        for (int i = 0; i < 3; i++) mid[3][i] = (b[i] + c[i]) / 2;
+        for (int i = 0; i < 3; i++) mid[4][i] = (c[i] + d[i]) / 2;
+        for (int i = 0; i < 3; i++) mid[5][i] = (b[i] + d[i]) / 2;
+        
+        // Create 4 tetrahedrons by subdivision.
+        divide_tetra(a, mid[0], mid[1], mid[2], m - 1);
+        divide_tetra(mid[0], b, mid[3], mid[5], m - 1);
+        divide_tetra(mid[1], mid[3], c, mid[4], m - 1);
+        divide_tetra(mid[2], mid[5], mid[4], d, m - 1);
+    } else {
+        // Draw triangle at the end of recursion.
+        tetrahedron(a, b, c, d);
+    }
 }
 
-void init() {
-        //glColor3f(1.0, 1.0, 1.0);
+void draw_coordinate_axises() {
+    glBegin(GL_LINES);
+    glColor3f(1, 0, 0);
+    glVertex3f(0, 0, 0);
+    glVertex3f(5, 0, 0);
 
+    glColor3f(0, 1, 0);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 5, 0);
+
+    glColor3f(0, 0, 1);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 0, 5);
+    glEnd();
+}
+
+void update(int time) {
+    cout << n << endl;
+
+    if (n > 0 && n < 6) {
+        if (!reversing) {
+            n++;
+        } else {
+            n--;
+        }
+    } else if (n == 0) {
+        reversing = false;
+        n++;
+    } else if (n == 6) {
+        reversing = true;
+        n--;
+    }
+    
+    glutPostRedisplay();
+    glutTimerFunc(1000, update, time);
 }
 
 void draw() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(0.1, 0.1, 0.1, 1.0);
 
-    glMatrixMode(GL_PROJECTION);
+    glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+    glOrtho(-1, 1, -1, 1, -1, 1);
+    //gluLookAt(1.5, 1.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glRotatef(x_theta, 1, 0, 0);
 
-    drawColorcube();
+    draw_coordinate_axises();
+    divide_tetra(v[0], v[1], v[2], v[3], n);
     glFlush();
+}
+
+void handle_input(unsigned char key, int x, int y) {
+    switch (key) {
+        case 'a':
+            x_theta += 5;
+            break;
+        case 'd':
+            x_theta -= 5;
+            break;
+        default:
+            break;
+    }
+
+    glutPostRedisplay();
 }
 
 
 int main(int argc, char* argv[]) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(0, 0);
-    glutCreateWindow("ColorCube");
+    glutCreateWindow("3D Gasket");
+
+    n = 0;
 
     glEnable(GL_DEPTH_TEST);
     glutDisplayFunc(draw);
+    glutKeyboardFunc(handle_input);
+    glutTimerFunc(100, update, 0);
+
     glutMainLoop();
 }
